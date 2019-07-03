407df42f86d75a47ea39933198d06d3735aa7cee
timerainbow <timerainbow@163.com>
2019-07-03 16:11:30 +0800

    typescript

---
+++ TypeScript/noHello.ts
@@ -0,0 +1,31 @@
+export class noHello{
+	//1:多行字符串
+	//var string = `abc
+	//def`;
+
+	//2:字符串模板
+	/*
+	var a = 'say goodbye';
+	function sayGoodBye(){
+		console.log('hello')
+	}
+	console.log(`you say:${a}`)
+	console.log(`you say:${sayGoodBye()}`)
+	*/
+
+	//3:自动拆分字符串
+	/*
+	function test(template,name,age){
+		console.log(template);
+		console.log(name);
+		console.log(age);
+	}
+
+	var myName  = "pig";
+	var getAge = function(){
+		return 2;
+	}
+
+	test`hello,my name is ${myName},i'm ${getAge()} years old`
+	*/
+}
