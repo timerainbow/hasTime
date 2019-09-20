b874252867e5303c4038a742d89046db5c584a4e
timerainbow <timerainbow@163.com>
2019-09-20 15:32:01 +0800

    add iconfont

--- DO_PROJECT/TravelProject/src/pages/home/Home.vue
+++ DO_PROJECT/TravelProject/src/pages/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
+        <home-header />
         <h1>home</h1>
     </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
-    
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
-<style scope>
-
-</style>
+<style scope></style>
