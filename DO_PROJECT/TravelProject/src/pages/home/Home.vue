0d3a2e08387b589c1d08ddf58f2903a91c00f787
timerainbow <timerainbow@163.com>
2019-09-24 10:43:44 +0800

    add

--- DO_PROJECT/TravelProject/src/pages/home/Home.vue
+++ DO_PROJECT/TravelProject/src/pages/home/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <home-header />
-        <home-swiper />
+        <home-header :city='city' />
+        <home-swiper :list='swiperList' />
         <home-icons />
         <home-recommend />
         <home-weekend />
@@ -14,6 +14,7 @@ import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
+import axios from 'axios'
 export default {
   name: 'Home',
   components: {
@@ -22,6 +23,30 @@ export default {
     HomeIcons,
     HomeRecommend,
     HomeWeekend
+  },
+  mounted () {
+    this.getHomeInfo()
+  },
+  data () {
+    return {
+      city: '',
+      swiperList: []
+    }
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json').then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      res = res.data
+      console.log(res)
+      if (res.ret && res.data) {
+        const data = res.data
+        this.city = '北京'
+        this.swiperList = data.swiperList
+        console.log(data.swiperList)
+      }
+    }
   }
 }
 </script>
--- DO_PROJECT/TravelProject/src/pages/home/components/Swiper.vue
+++ DO_PROJECT/TravelProject/src/pages/home/components/Swiper.vue
@@ -1,11 +1,8 @@
 <template>
     <div class="wrapper">
         <swiper :options="swiperOption">
-            <swiper-slide>
-                <img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20199/3ff47fa622d07edad492c2859a5ad32f.jpg_750x200_3df30168.jpg" />
-            </swiper-slide>
-            <swiper-slide>
-                <img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20199/0fa39f9f5e66189e85b5c6e54278587d.jpg_750x200_86c8f2d8.jpg" />
+            <swiper-slide v-for="item in list" :key="item.id">
+                <img class="swiper-img" src="item.imgUrl" />
             </swiper-slide>
             <div class="swiper-pagination" slot="pagination"></div>
         </swiper>
@@ -15,6 +12,9 @@
 <script>
 export default {
   name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
   data () {
     return {
       swiperOption: {
